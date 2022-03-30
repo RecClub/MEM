82565bd0b61b981e87f986b906344c3084a44694
Yu Chen Zhao <yczhao0916@gmail.com>
2022-03-30 18:25:00 -0400

    Setup automatic light/dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
 import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { CssBaseline } from '@mui/material';
+
+import useDarkMode from './hooks/useDarkMode';
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  const theme = createTheme({
+    palette: {
+      mode: useDarkMode() ? 'dark' : 'light',
+    }
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <Button variant="contained">Hello World</Button>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ client/src/hooks/useDarkMode.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+const matchDark = '(prefers-color-scheme: dark)';
+
+function useDarkMode() {
+  const [isDark, setIsDark] = useState(
+    () => window.matchMedia && window.matchMedia(matchDark).matches
+  );
+  
+  useEffect(() => {
+    const matcher = window.matchMedia(matchDark);
+    const onChange = ({ matches }) => setIsDark(matches);
+    matcher.addEventListener("change", onChange);
+    return () => {
+      matcher.removeEventListener(onChange);
+    }
+  }, [setIsDark]);
+
+  return isDark;
+}
+
+export default useDarkMode;
