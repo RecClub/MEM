3161a30947a4bd1726fc14643ee94f9860e4a32d
Haresh Sivasuntharampillai <51726426+Haresh-Sivapalan@users.noreply.github.com>
2022-04-08 03:52:20 -0400

    Adding Class log

--- client/src/components/MemberLog.js
+++ client/src/components/MemberLog.js
@@ -1,15 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 
-import jsonDB from '../apis/jsonDB';
+import jsonDB from "../apis/jsonDB";
 
 const MemberLog = () => {
   let [users, setUsers] = useState();
 
+  const [value, setValue] = React.useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   useEffect(() => {
     const fetchUsers = async () => {
-      const data = await jsonDB.get('/users');
+      const data = await jsonDB.get("/classlog");
       setUsers(data.data);
     };
 
@@ -20,18 +30,30 @@ const MemberLog = () => {
 
   let gridData = {
     columns: [
-      { field: 'id', hide: true },
-      { field: 'name', headerName: 'Name' },
-      { field: 'phonenum', headerName: 'Phone Number' },
-      { field: 'paid', headerName: 'Paid' },
-      { field: 'address', headerName: 'Address' },
+      { field: "id", hide: true },
+      { field: "attending", headerName: "Attending" },
+      { field: "phonenum", headerName: "Phone Number" },
+      { field: "paid", headerName: "Paid" },
+      { field: "address", headerName: "Address" },
     ],
     rows: users,
   };
 
   return (
     <div>
-      <div style={{ display: 'flex', maxWidth: 600, minHeight: 300 }}>
+      <div style={{ display: "flex", maxWidth: 600, minHeight: 300 }}>
+        <FormControl fullWidth>
+          <InputLabel>Classes</InputLabel>
+          <Select
+            value={value}
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Class 1</MenuItem>
+            <MenuItem value={20}>Class 2</MenuItem>
+            <MenuItem value={30}>Class 3</MenuItem>
+          </Select>
+        </FormControl>
+
         <DataGrid
           autoHeight
           components={{ Toolbar: GridToolbar }}
