c53f81e1fda9349099d78806ec2128c6912ce151
mijalshrestha <mshrestha@ryerson.ca>
2022-04-13 13:41:51 -0400

    assign class to coach

---
+++ client/src/components/CoachAdd.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import jsonDB from "../apis/jsonDB";
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import Button from '@mui/material/Button';
+
--- client/src/components/CoachList.js
+++ client/src/components/CoachList.js
@@ -1,14 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext} from "react";
 import jsonDB from "../apis/jsonDB";
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Button from '@mui/material/Button';
-
+import userContext from '../contexts/UserContext';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 
 const CoachList = () => {
-
+    let loggedInUser = useContext(userContext).user;
     let [users, setUsers] = useState();
     let [selectedUsers, setSelectedUsers] = useState([]);
+    let [selectedClasses, setselectedClasses] = useState([]);
+    let [classes, setClasses] = useState();
+
+    
 
     const handleSelectionChange = (ids) => {
         if (!ids) return;
@@ -16,13 +21,47 @@ const CoachList = () => {
         setSelectedUsers(users.filter((user) => selectedIDs.has(user.id)));
       };
 
+      const handleClassSelectionChange = (ids) => {
+        if (!ids) return;
+        const selectedIDs = new Set(ids);
+        setselectedClasses(classes.filter((classes) => selectedIDs.has(classes.id)));
+      };
+
     const handleDeleteCoach = async () => {
         let promises = selectedUsers.map((user) => {
             return jsonDB.delete(`/users/${user.id}`);
         });
+        // handleSendMessage();
         await Promise.all(promises);
         window.location.reload(false);
     }
+
+    const handleClasses = async () => {
+      let promises = selectedUsers.map((user) => {
+          selectedClasses.forEach((c) => {
+            user.class[c.id] = true;
+           
+          });
+          return jsonDB.put(`/users/${user.id}`,user)
+      });
+      // handleSendMessage();
+      await Promise.all(promises);
+      window.location.reload(false);
+    }
+
+    const handleSendMessage = async () => {
+      let message = "You have been removed from all you classes";
+      // setTextValue('');
+      selectedUsers.forEach(async (user) => {
+        const data = await jsonDB.get(`/user_messages/${user.id}`);
+        const user_messages = data.data;
+  
+        user_messages.messages = [...user_messages.messages, { message, date: new Date(), sender: loggedInUser.name, senderID: loggedInUser.id, read: false}];
+        jsonDB.put(`/user_messages/${user.id}`, user_messages);
+      });
+  
+      // handleDialogOpen();
+    };
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -34,11 +73,24 @@ const CoachList = () => {
       }, []);
 
 
+  
+
+      useEffect(() => {
+        const fetchClass = async () => {
+          const data = await jsonDB.get("/classes");
+          setClasses(data.data);
+        };
+    
+        fetchClass();
+      }, []);
+      
       if (!users) return <div>Loading...</div>;
 
       let filterlist = users.filter( x => {
         return (x.role === "Coach") 
       } )
+
+      const classList = classes
 
       let gridData = {
         columns: [
@@ -46,6 +98,15 @@ const CoachList = () => {
           { field: "name", headerName: "Name" },
         ],
         rows: filterlist,
+      };
+
+      
+      let gridData2 = {
+        columns: [
+          { field: "id"},
+          { field: "name", headerName: "Name" },
+        ],
+        rows: classList,
       };
 
 
@@ -59,8 +120,20 @@ const CoachList = () => {
                 onSelectionModelChange={handleSelectionChange}
                 {...gridData}
             />
-                      <Button variant="contained" endIcon={<DeleteIcon /> }onClick={handleDeleteCoach} >
+                      <Button variant="contained" endIcon={<DeleteIcon /> } onClick={handleDeleteCoach} >
                         Delete
+                     </Button>
+
+                <DataGrid
+                autoHeight
+                checkboxSelection={true}
+                components={{ Toolbar: GridToolbar }}
+                onSelectionModelChange={handleClassSelectionChange}
+                {...gridData2}
+            />
+
+                      <Button variant="contained" endIcon={<PersonAddAlt1Icon /> } onClick={handleClasses} >
+                        Assign Class
                      </Button>
 
             </div>
