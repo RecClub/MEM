436d6df6560add78c89722e9ff97ee154a004462
Sher <76260479+shersafi@users.noreply.github.com>
2022-04-15 17:39:40 -0400

    database fix

--- client/src/components/PaymentInvoice.js
+++ client/src/components/PaymentInvoice.js
@@ -19,13 +19,6 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
 
 const Transition = React.forwardRef(function Transition(props, ref) {
